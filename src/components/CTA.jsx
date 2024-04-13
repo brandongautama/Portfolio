9074887b18ca6faafd5bcf4f4ff527c0799b708a
Brandon Gautama <bgautama@amazon.com>
2024-04-13 16:33:29 -0700

    complete About page

---
+++ src/components/CTA.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CTA = () => {
+  return (
+    <section className="cta">
+      <p className="cta-text">
+        Have a project in mind? <br className="sm:block hidden" />
+        Let's build something together
+      </p>
+      {/* <Link to="/contact" className="button"></Link> */}
+    </section>
+  );
+};
+
+export default CTA;
