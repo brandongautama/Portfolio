70fed465424e9269d5a2afb0e060d2dc45c0f76a
Brandon Gautama <bgautama@amazon.com>
2024-04-14 14:09:14 -0700

    add animation to hamburger menu

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import { Squash as Hamburger } from "hamburger-react";
+import { AnimatePresence, motion } from "framer-motion";
 import { routes } from "../routes";
 import { useClickAway } from "react-use";
 
@@ -22,40 +23,48 @@ export const NavMobile = () => {
         color="#0072ff"
         direction="right"
       />
-      {isOpen && (
-        <div className="fixed left-0 shadow-4xl -0 top-[3.5rem] p-5 pt-0 border-b border-b-white/20">
-          <ul className="grid gap-2 flex-col">
-            {isOpen && (
-              <div className="fixed left-0 shadow-4xl rounded-lg right-0 top-[3.5rem] p-3 bg-slate-200/100 border-b border-b-white/20">
-                <ul className="grid gap-2">
-                  {routes.map((route) => {
-                    return (
-                      <li
-                        key={route.title}
-                        className="w-full p-[0.08rem] rounded-xl bg-gradient-to-tr bg-slate-300/20 via-neutral-150 to-neutral-100"
-                      >
-                        <a
-                          onClick={() => setOpen((prev) => !prev)}
-                          className={
-                            "flex items-center justify-between w-full p-5 rounded-xl bg-neutral-100"
-                          }
-                          href={route.href}
-                        >
-                          <span></span>
-                          <span className="flex gap-1 text-lg font-poppins text-black">
-                            {route.title}
-                          </span>
-                          <span></span>
-                        </a>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
-            )}
-          </ul>
-        </div>
-      )}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            className="fixed left-0 shadow-4xl rounded-xl right-0 top-[3.5rem] p-2 mt-4 bg-slate-200/100 border-b border-b-white/20"
+          >
+            <ul className="grid gap-2">
+              {routes.map((route, idx) => {
+                return (
+                  <motion.li
+                    initial={{ scale: 0, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    transition={{
+                      type: "spring",
+                      stiffness: 260,
+                      damping: 20,
+                      delay: 0.1 + idx / 10,
+                    }}
+                    key={route.title}
+                    className="w-full p-[0.08rem] rounded-xl bg-gradient-to-tr bg-white via-neutral-100 to-neutral-100"
+                  >
+                    <a
+                      onClick={() => setOpen((prev) => !prev)}
+                      className={
+                        "flex items-center justify-between w-full p-5 rounded-xl bg-neutral-0"
+                      }
+                      href={route.href}
+                    >
+                      <span />
+                      <span className="flex gap-1 text-lg">{route.title}</span>
+                      <span />
+                    </a>
+                  </motion.li>
+                );
+              })}
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
