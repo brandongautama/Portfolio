34dc8f40a38dd2ea83badcc65df107f9e52f4527
Brandon Gautama <bgautama@amazon.com>
2024-04-13 17:24:04 -0700

    add Projects page

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 const CTA = () => {
+  const [copyStatus, setCopyStatus] = useState(false); // To indicate if the text was copied
+
+  const onCopyText = () => {
+    setCopyStatus(true);
+    setTimeout(() => setCopyStatus(false), 2000); // Reset status after 2 seconds
+  };
+
   return (
     <section className="cta">
       <p className="cta-text">
@@ -8,6 +16,12 @@ const CTA = () => {
         Let's build something together
       </p>
       {/* <Link to="/contact" className="button"></Link> */}
+      <div>
+        <CopyToClipboard text="brandongautama15@gmail.com" onCopy={onCopyText}>
+          <button className="btn">brandongautama15@gmail.com</button>
+        </CopyToClipboard>
+        {copyStatus && <div className="alert-success mt-2">Email copied!</div>}
+      </div>
     </section>
   );
 };
